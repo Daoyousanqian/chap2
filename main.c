7e1ef3991ec1e3099ccb269c4ee334b251032053
Daoyousanqian <shengquan.wang@campus.tu-berlin.de>
2020-03-03 13:54:06 -0800

    add pointers

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ d_ptr_to_char  a1, b1;                 // eg, equal to ******** char * a1, b1; *
 int const  random1;
 const int  random2;
  
-int *pi ;
-int const *pci ;
-int *const cpi ;
-int const *const cpci ;
+int *pi ;                // an ordinary pointer;
+int const *pci ;    //  can change pointer's value but not allow to change the value it points **** *pci = 30; wrong **** pci = & num3; right ********
+int *const cpi ;    //  allow the change the vlaue it point but not allow to chage pointer's value;
+int const *const cpci ;  // both do not allow; 
 
 
 
@@ -41,7 +41,7 @@ int main(void){
    
 	printf("the number of pi is  :%d\n", *pi);
 	printf("the number of pi is  :%d\n", *pci);
-	num2 = 20;
+	pci = &num3;
 	printf("the number of pi is  :%d\n", *pci);
 	
 	 char a[] = "asad"; // the difference between '  ' for char and  "  " is for string. 
